c7fa114650468cb55beb108799b9b20542d4e94c
AegisTelesoft <egillk@telesoftas.com>
2017-06-07 09:57:32 +0300

    Fixed bugs and issues

--- src/app.kt
+++ src/app.kt
@@ -15,37 +15,38 @@ data class FileJson(
         val lastModified: Long
 )
 
-fun main(args: Array<String>) {
+fun scanDirectory(directory: String, treeNode: DirJson) {
+    val file = File(directory)
+    val fileWalker = file.walk().maxDepth(1)
 
-    // args[0] directory as a command line argument
-    val file = File(args[0]);
-
-    // Creating File.walk instance so I could Iterate through all files in specified directory
-    val fileWalker = file.walk();
-
-    val root: DirJson = DirJson("", 0, 0);
-    var currentDir: DirJson = root;
-
-    // Iterating through specified directory
     fileWalker.forEach {
-        if(currentDir.name == "") {
-            currentDir.name = it.name;
-            currentDir.size = it.length();
-            currentDir.lastModified = it.lastModified();
-        }
-        else if(it.isDirectory) {
-            currentDir.directories.add(DirJson(it.name, it.length(), it.lastModified()));
-            currentDir = currentDir.directories.last();
+        if(it.isDirectory) {
+            if(directory != it.path) {
+                treeNode.directories.add(DirJson(it.name, it.length(), it.lastModified()))
+                scanDirectory(it.path, treeNode.directories.last())
+            }
+            else{
+                treeNode.name = it.name;
+                treeNode.size = it.length();
+                treeNode.lastModified = it.lastModified();
+            }
         }
         else {
-            currentDir.files.add(FileJson(it.name, it.length(), it.lastModified()));
+            treeNode.files.add(FileJson(it.name, it.length(), it.lastModified()))
         }
     }
+}
+
+fun main(args: Array<String>) {
+
+    val root: DirJson = DirJson("", 0, 0);
+
+    scanDirectory(args[0], root);
 
     // Craeting `data Class to Json` object mapper
     val mapper = jacksonObjectMapper();
 
-    // Mapping `root` object to Json as a string (this operation fails to compile with error mentioned above)
+    // Mapping `root` object to Json as a string
     val json = mapper.writeValueAsString(root);
 
     print(json);
