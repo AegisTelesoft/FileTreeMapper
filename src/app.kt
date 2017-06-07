bb3bf62214a15fd155b9438996de910c5f1a6896
AegisTelesoft <egillk@telesoftas.com>
2017-06-07 09:22:45 +0300

    Added some comments

--- src/app.kt
+++ src/app.kt
@@ -17,12 +17,16 @@ data class FileJson(
 
 fun main(args: Array<String>) {
 
+    // args[0] directory as a command line argument
     val file = File(args[0]);
+
+    // Creating File.walk instance so I could Iterate through all files in specified directory
     val fileWalker = file.walk();
 
     val root: DirJson = DirJson("", 0, 0);
     var currentDir: DirJson = root;
 
+    // Iterating through specified directory
     fileWalker.forEach {
         if(currentDir.name == "") {
             currentDir.name = it.name;
@@ -38,13 +42,16 @@ fun main(args: Array<String>) {
         }
     }
 
+    // Craeting `data Class to Json` object mapper
     val mapper = jacksonObjectMapper();
 
     /*
-    * Error:Kotlin: Supertypes of the following classes cannot be resolved. Please make sure you have the required dependencies in the classpath:
-    * class com.fasterxml.jackson.databind.ObjectMapper, unresolved supertypes: ObjectCodec, Versioned
+    * Error:Kotlin: Supertypes of the following classes cannot be resolved. Please make sure you have the required
+    * dependencies in the classpath:class com.fasterxml.jackson.databind.ObjectMapper, unresolved supertypes:
+    * ObjectCodec, Versioned
     */
 
+    // Mapping `root` object to Json as a string (this operation fails to compile with error mentioned above)
     val json = mapper.writeValueAsString(root);
 
     print(json);
